0c2cad582da264223d30d49a9e4444319d744dda
Lndqw12 <COMP222012016@mail.ru>
2017-03-22 22:43:53 +0300

    10comproblemget

---
+++ secret.php
@@ -0,0 +1,17 @@
+<?php
+	if($_GET['key'] != '1287975')
+	{
+		header('location: main.html.php;');
+		exit();
+	}
+?>
+<!Doctype html>
+<html>
+	<head>
+		<title>Secret</title>
+		<meta charset="utf-8" />
+	</head>
+		<body>
+			<h1 style="color:red; text-align:center; font-size:2em; font-family:impact">SECRET</h1>
+		</body>
+</html>

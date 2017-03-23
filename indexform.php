53da59de0f108a49f8432b642249b7c0e0e72f07
Lndqw12 <COMP222012016@mail.ru>
2017-03-23 23:40:23 +0300

    12bd

--- indexform.php
+++ indexform.php
@@ -1,5 +1,12 @@
 <?php
-	$rate = 30;
+	//подключение и извлечение из бд
+	mysql_connect('localhost', 'root', '');
+	mysql_select_db('quick');
+	
+	$result = mysql_query("SELECT * FROM rates WHERE name='USD'");
+	$row = mysql_fetch_assoc($result);
+	
+	$rate = $row['value'];
 	
 	if(isset($_POST['bucks']))
 		$bucks = $_POST['bucks'];
